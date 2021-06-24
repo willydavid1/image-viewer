8616c5658c80123c94280de588595239f70a2fe9
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-06-23 22:56:43 -0500

    Image loading logic finished

--- components/pictureList/index.tsx
+++ components/pictureList/index.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { IoScanOutline, IoList } from 'react-icons/io5'
+import axios from 'axios'
+import { store as storeNotifications } from 'react-notifications-component'
+import Image from 'next/image'
 
 interface Category {
   type: string
   label: string
 }
+
+interface Pictures {
+  isLoading: boolean
+  error: any
+  data: Array<any>
+  page: number
+}
+
+const API_BASE_URL = 'https://api.unsplash.com/photos/'
 
 const categories: Category[] = [
   {
@@ -33,46 +45,118 @@ const PictureList = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>(
     categories[0].type
   )
+  const [pictures, setPictures] = useState<Pictures>({
+    isLoading: false,
+    error: null,
+    data: [],
+    page: 0
+  })
+
+  const getData = async () => {
+    setPictures({
+      ...pictures,
+      isLoading: true
+    })
+
+    try {
+      const newPage = pictures.page + 1
+      const { data } = await axios.get(
+        `${API_BASE_URL}?page=${newPage}&per_page=20&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID_UNPLASH}`
+      )
+      console.log(data)
+      setPictures({
+        ...pictures,
+        data: [...pictures.data, ...data],
+        isLoading: false,
+        page: newPage
+      })
+      storeNotifications.addNotification({
+        title: 'Images Uploaded',
+        message: `Page #${newPage}`,
+        type: 'success',
+        insert: 'top',
+        container: 'top-right',
+        animationIn: ['animate__animated', 'animate__fadeIn'],
+        animationOut: ['animate__animated', 'animate__fadeOut'],
+        dismiss: {
+          duration: 2000,
+          onScreen: true
+        }
+      })
+    } catch (error) {
+      storeNotifications.addNotification({
+        title: 'Error Loading Images',
+        message: '=(',
+        type: 'danger',
+        insert: 'top',
+        container: 'top-right',
+        animationIn: ['animate__animated', 'animate__fadeIn'],
+        animationOut: ['animate__animated', 'animate__fadeOut'],
+        dismiss: {
+          duration: 5000,
+          onScreen: true
+        }
+      })
+      setPictures({
+        ...pictures,
+        isLoading: false,
+        error
+      })
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div>
-      <div className='flex items-center flex-col'>
-        <div className='space-x-4 text-pink-600 my-5'>
-          <button className='text-2xl'>
-            <IoScanOutline />
-          </button>
-          <button className='text-2xl'>
-            <IoList />
-          </button>
-        </div>
-
-        <div className='flex text-xs mb-5'>
-          {categories.map((ctg: Category) => (
-            <button
-              onClick={() => {
-                setSelectedCategory(ctg.type)
-              }}
-              key={ctg.type}
-              className={`py-2 px-4 flex justify-center items-center capitalize hover:bg-pink-400 hover:text-white ${
-                selectedCategory === ctg.type ? 'bg-pink-500 text-white' : ''
-              }`}
-            >
-              {ctg.label}
-            </button>
-          ))}
-        </div>
-
-        <div className="grid grid-cols-3 gap-2 w-full">
-          <div className="col-span-2 bg-pink-400 p-10 rounded">1</div>
-          <div className="bg-pink-400 p-10 rounded">2</div>
-          <div className="row-span-3 bg-pink-400 p-10 rounded">3</div>
-          <div className="bg-pink-400 p-10 rounded">4</div>
-          <div className="bg-pink-400 p-10 rounded">5</div>
-          <div className="bg-pink-400 p-10 rounded col-span-2">6</div>
-          <div className="col-span-2 bg-pink-400 p-10 rounded">7</div>
-          <div className="bg-pink-400 p-10 rounded">8</div>
-          <div className="col-span-2 bg-pink-400 p-10 rounded">9</div>
-        </div>
+    <div className='flex items-center flex-col'>
+      <div className='space-x-4 text-pink-600 my-5'>
+        <button className='text-2xl'>
+          <IoScanOutline />
+        </button>
+        <button className='text-2xl'>
+          <IoList />
+        </button>
       </div>
+
+      <div className='flex text-xs mb-5'>
+        {categories.map((ctg: Category) => (
+          <button
+            onClick={() => {
+              setSelectedCategory(ctg.type)
+            }}
+            key={ctg.type}
+            className={`py-2 px-4 flex justify-center items-center capitalize hover:bg-pink-400 hover:text-white ${
+              selectedCategory === ctg.type ? 'bg-pink-500 text-white' : ''
+            }`}
+          >
+            {ctg.label}
+          </button>
+        ))}
+      </div>
+
+      <div className='grid-container w-full'>
+        {pictures.data.map((img, i) => (
+          <div key={img.id} className={`${i % 2 === 0 ? 'tall' : ''} grid-item relative`}>
+            <Image
+              src={img.urls.small}
+              alt={img.alt_description}
+              layout='fill'
+              objectFit="contain"
+              className="transition-transform"
+            />
+          </div>
+        ))}
+      </div>
+
+      {
+        pictures.isLoading && <h2 className="text-pink-700 text-xl my-12">Loading...</h2>
+      }
+
+      <button onClick={getData} className="my-12 py-3 px-5 bg-pink-500 text-white hover:bg-pink-400">
+        Show More
+      </button>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
+import 'react-notifications-component/dist/theme.css'
+
 import type { AppProps } from 'next/app'
 
+import ReactNotification from 'react-notifications-component'
 import Layout from 'components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <ReactNotification />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
   
 }
